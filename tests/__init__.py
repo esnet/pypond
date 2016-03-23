aeb88172e21532859beb82e72586c578d78ba0ce
Monte Goode <MMGoode@lbl.gov>
2016-03-23 08:26:23 -0700

    Add in nose/etc.

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Import all tests to run at once.
-"""
-# pylint: disable=wildcard-import, unused-wildcard-import
-from .event_test import *
-from .index_test import *
-from .range_test import *
-from .series_test import *
