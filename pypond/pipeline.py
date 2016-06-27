80d2f5b5671e24b08e252eaca96fef3de00a083d
Monte Goode <MMGoode@lbl.gov>
2016-06-27 12:26:38 -0700

    Pull over dox from pipeline.js and do the API dox on the Pipeline class stub. Might as well get the basic dox done before implementation since I'll be the first consumer.

--- pypond/pipeline.py
+++ pypond/pipeline.py
@@ -55,11 +55,11 @@ class Runner(PypondBase):  # pylint: disable=too-few-public-methods
         The output driving this runner
     """
 
-    def __init__(self, pipeline, output):
+    def __init__(self, pline, output):
         """Create a new batch runner"""
         super(Runner, self).__init__()
 
-        self._pipeline = pipeline
+        self._pipeline = pline
         self._output = output
 
         # We use the pipeline's chain() function to walk the
@@ -96,7 +96,7 @@ class Runner(PypondBase):  # pylint: disable=too-few-public-methods
         pass
 
 
-class Pipeline(PypondBase):
+class Pipeline(PypondBase):  # pylint: disable=too-many-public-methods
     """
     Build a new Pipeline.
 
@@ -167,12 +167,25 @@ class Pipeline(PypondBase):
     # Pipeline mutations
 
     def _set_in(self, input):
+        """
+        Setting the In for the Pipeline returns a new Pipeline.
+        """
         raise NotImplementedError
 
     def _set_first(self, i):
+        """
+        Set the first processing node pointed to, returning
+        a new Pipeline. The original pipeline will still point
+        to its orginal processing node.
+        """
         raise NotImplementedError
 
     def _set_last(self, i):
+        """
+        Set the last processing node pointed to, returning
+        a new Pipeline. The original pipeline will still point
+        to its orginal processing node.
+        """
         raise NotImplementedError
 
     def _append(self, processor):
@@ -181,22 +194,147 @@ class Pipeline(PypondBase):
     # Pipeline state chained methods
 
     def window_by(self, win):
+        """
+        Set the window, returning a new Pipeline. The argument here
+        is an object with {type, duration}.
+
+        Window *win* may be:
+
+        * A fixed interval: "fixed"
+        * A calendar interval: "day," "month" or "year"
+
+        Duration is of the form:
+
+        * "30s" or "1d" etc - supports seconds (s), minutes (m), hours (h),
+          days (d)
+
+        Parameters
+        ----------
+        win : string
+            See above.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def clear_window(self):
+        """
+        Remove windowing from the Pipeline. This will
+        return the pipeline to no window grouping. This is
+        useful if you have first done some aggregated by
+        some window size and then wish to collect together
+        the all resulting events.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
     def group_by(self, key):
+        """
+        Sets a new groupBy expression. Returns a new Pipeline.
+
+        Grouping is a state set on the Pipeline. Operations downstream
+        of the group specification will use that state. For example, an
+        aggregation would occur over any grouping specified.
+
+
+        Parameters
+        ----------
+        key : function, list or string
+            The key tro group by. You can group_by using a function,
+            a field_spec (field name or dot.delimited.path) or an array
+            of field_specs
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
     def clear_group_by(self):
+        """
+        Remove the grouping from the pipeline. In other words
+        recombine the events.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
     def emit_on(self, trigger):
+        """
+        Sets the condition under which an accumulated collection will
+        be emitted. If specified before an aggregation this will control
+        when the resulting event will be emitted relative to the
+        window accumulation. Current options are:
+
+        * to emit on every event, or
+        * just when the collection is complete, or
+        * when a flush signal is received, either manually calling done(),
+          or at the end of a bounded source.
+
+        The strings indicating how to trigger how a Collection should
+        be emitted - can be:
+
+        * "eachEvent" - when a new event comes in, all currently maintained
+          collections will emit their result.
+        * "discard" - when a collection is to be discarded, first it will
+          emit. But only then.
+        * "flush" - when a flush signal is received.
+
+        The difference will depend on the output you want, how often
+        you want to get updated, and if you need to get a partial state.
+        There's currently no support for late data or watermarks. If an
+        event passes comes in after a collection window, that collection
+        is considered finished.
+
+
+        Parameters
+        ----------
+        trigger : string
+            See above
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
+    # I/O
+
     def from_source(self, src):
-        """originally named from() in JS code."""
+        """
+        Note: originally named from() in JS code.
+
+        The source to get events from. The source needs to be able to
+        iterate its events using `for..of` loop for bounded Ins, or
+        be able to emit() for unbounded Ins. The actual batch, or stream
+        connection occurs when an output is defined with `to()`.
+
+        Pipelines can be chained together since a source may be another
+        Pipeline.
+
+
+        Parameters
+        ----------
+        src : BoundedIn, UnboundedIn or Pipeline
+            The source for the Pipeline, or another Pipeline.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def to_event_list(self):
@@ -205,46 +343,286 @@ class Pipeline(PypondBase):
     def to_keyed_collections(self):
         raise NotImplementedError
 
-    def to(self):
+    def to(self, out, observer, options=None):  # pylint: disable=invalid-name, dangerous-default-value
+        """
+        Sets up the destination sink for the pipeline.
+
+        For a batch mode connection, i.e. one with a Bounded source,
+        the output is connected to a clone of the parts of the Pipeline dependencies
+        that lead to this output. This is done by a Runner. The source input is
+        then iterated over to process all events into the pipeline and though to the Out.
+
+        For stream mode connections, the output is connected and from then on
+        any events added to the input will be processed down the pipeline to
+        the out.
+
+        ::
+
+            p = Pipeline()
+
+            XXX FLESH OUT EXAMPLE
+
+        NOTE: arg list has been changed from the ordering in the JS source
+        to conform to python convention.
+
+        Parameters
+        ----------
+        out : EventOut, etc instance
+            The output.
+        observer : function or instance
+            The observer.
+        options : dict, optional
+            Options.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
-    def count(self):
+    def count(self, observer, force=True):
+        """
+        Outputs the count of events.
+
+        Parameters
+        ----------
+        observer : function
+            The callback function. This will be passed the count, the window_key
+            and the group_by_key.
+        force : bool, optional
+            Flush at the end of processing batch events, output again with possibly
+            partial result
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
-    def offset_by(self, by, field_spec):
+    def offset_by(self, offset_by, field_spec):
+        """
+        Processor to offset a set of fields by a value. Mostly used for
+        testing processor and pipeline operations with a simple operation.
+
+        Parameters
+        ----------
+        offset_by : int, float
+            The amout to offset by.
+        field_spec : string, list
+            The fields
+
+        Returns
+        -------
+        Pipeline
+            The modified Pipeline.
+        """
         raise NotImplementedError
 
     def aggregate(self, fields):
+        """
+        Uses the current Pipeline windowing and grouping
+        state to build collections of events and aggregate them.
+
+        IndexedEvents will be emitted out of the aggregator based
+        on the `emitOn` state of the Pipeline.
+
+        To specify what part of the incoming events should
+        be aggregated together you specify a `fields`
+        object. This is a map from fieldName to operator.
+
+        ::
+
+            XXX FLESH OUT EXAMPLE
+
+
+        Parameters
+        ----------
+        fields : instance
+            Fields and operators to be aggregated
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
     def as_events(self, options):
+        """
+        Converts incoming TimeRangeEvents or IndexedEvents to
+        Events. This is helpful since some processors will
+        emit TimeRangeEvents or IndexedEvents, which may be
+        unsuitable for some applications.
+
+        There are three options:
+
+        1. use the beginning time (options = {'alignment': "lag"})
+        2. use the center time (options = {'alignment': "center"})
+        3. use the end time (options = {'alignment': "lead"})
+
+        Parameters
+        ----------
+        options : dict
+            The options, see above.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def map(self, op):  # pylint: disable=invalid-name
+        """
+        Map the event stream using an operator.
+
+
+        Parameters
+        ----------
+        op : function
+            A function that returns a new Event.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def filter(self, op):  # pylint: disable=invalid-name
+        """
+        Filter the event stream using an operator
+
+        Parameters
+        ----------
+        op : function
+            A function that returns True or False
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
     def select(self, field_spec):
+        """
+        Select a subset of columns.
+
+        Parameters
+        ----------
+        field_spec : list, string
+            The columns to include in the output.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def collapse(self, field_spec, name, reducer, append):
+        """
+        Collapse a subset of columns using a reducer function.
+
+
+        Parameters
+        ----------
+        field_spec : list, string
+            The columns to collapse into the output.
+        name : string
+            The resulting output column's name.
+        reducer : function
+            Function to use to do the reduction.
+        append : bool
+            Add the new column to the existing ones, or replace them.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def take(self, limit):
+        """
+        Take events up to the supplied limit, per key.
+
+        Parameters
+        ----------
+        limit : int
+            Integer number of events to take.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline.
+        """
         raise NotImplementedError
 
     def as_time_range_events(self, options):
+        """
+        Converts incoming Events or IndexedEvents to TimeRangeEvents.
+
+        There are three option for alignment:
+
+        1. time range will be in front of the timestamp (options = {'alignment': "front"})
+        2. time range will be centered on the timestamp (options = {'alignment': "center"})
+        3. time range will be positoned behind the timestamp (options = {'alignment': "behind"})
+
+        The duration is of the form "1h" for one hour, "30s" for 30 seconds and so on.
+
+        Parameters
+        ----------
+        options : dict
+            Options - see above.
+
+        Returns
+        -------
+        Pipeline
+            The Pipeline
+        """
         raise NotImplementedError
 
     def as_indexed_events(self, options):
+        """
+        Converts incoming Events to IndexedEvents.
+
+        Note: It isn't possible to convert TimeRangeEvents to IndexedEvents.
+
+
+        Parameters
+        ----------
+        options : dict
+            Contains the conversion options. In this case, the duration string
+            of the Index is expected. Must contain the key 'duration' and the
+            duration string is of the form "1h" for one hour, "30s" for 30
+            seconds and so on.
+
+        Returns
+        -------
+        TYPE
+            Description
+
+        Raises
+        ------
+        NotImplementedError
+            Description
+
+
+        """
         raise NotImplementedError
 
 # module functions
 
-def pipeline(args):
-    return Pipeline(args)
+# def pipeline(args):
+#     return Pipeline(args)
+
 
 def is_pipeline(pline):
+    """Boolean test to see if something is a Pipeline instance."""
+    return isinstance(pline, Pipeline)
+
     return isinstance(pline, Pipeline)
